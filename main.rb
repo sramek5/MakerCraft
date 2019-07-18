22d890337437dd11820893d9bc6f7aa0ecbb83ba
sramek5 <53052347+sramek5@users.noreply.github.com>
2019-07-18 22:13:58 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -18,4 +18,4 @@ state.upcase!
 #upcase uses only big letters
 
 puts "Your name is #{first_name} #{last_name} and you live in #{city}, #{state}!"
-#whole pogram works fine, it s necesarry to aswer it in the command prompt 
+#whole pogram works fine, it s necsesary to aswer it in the command prompt 
